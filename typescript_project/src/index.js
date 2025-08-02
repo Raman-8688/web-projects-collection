30964e53ae1a1f7889bb73fe663c28a121c55fe1
Raman <ramanms8688@gmail.com>
2025-08-02 19:42:21 +0530

    Added TypeScript project with line endings fix

---
+++ typescript_project/src/index.js
@@ -0,0 +1,13 @@
+//var username="jonn Doe";
+//document.write("hello "+username+" welocome to my frist typescript project");
+//console.log("Hello "+username);
+function Demo() {
+    var x = 10;
+    if (x == 10) {
+        var y;
+        y = 20;
+        console.log("x=" + x + "\n" + "y=" + y);
+    }
+    console.log("x=" + x + "\n" + "y=" + y);
+}
+Demo();
---
+++ typescript_project/src/index.ts
@@ -0,0 +1,16 @@
+//var username="jonn Doe";
+//document.write("hello "+username+" welocome to my frist typescript project");
+//console.log("Hello "+username);
+
+
+function Demo(){
+    let x=10;
+    if(x==10){
+        let y;
+        y=20;
+       
+        console.log("x="+x+ "\n" +"y="+y);
+    }
+    console.log("x="+x+ "\n"+"y="+y);
+}
+Demo()
